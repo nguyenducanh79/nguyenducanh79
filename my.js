ed55ef6f409b0adc5a61c128b1500a96ae223903
nguyenducanh79 <ducanhbatha12345@gmail.com>
2021-12-09 16:26:45 +0700

    nguyenducanh

---
+++ my.js
@@ -0,0 +1,13 @@
+ let vatly
+ let dialy
+ let sinhhoc
+
+ vatly = prompt ( 'nhap diem vat ly' ) ;
+ dialy = prompt ( 'nhap diem dia ly ' );
+ sinhhoc = prompt ( 'nhap diem sinh hoc ');
+
+ vatly=parseInt(vatly)
+ dialy=parseInt(dialy)
+ sinhhoc=parseInt(sinhhoc)
+ alert  ("diemtrungbinh; "+"\n" +(vatly+dialy+sinhhoc)/3 )
+
